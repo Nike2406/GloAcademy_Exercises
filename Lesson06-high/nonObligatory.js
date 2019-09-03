53e2134da78f5f05c80d331c7c0bfc78c78ac182
Nikita Bukin <locus2@mail.ru>
2019-09-04 00:03:59 +0300

    add an exercise

--- Lesson06-high/nonObligatory.js
+++ Lesson06-high/nonObligatory.js
@@ -1,12 +1,14 @@
-let user = {
-  name: "John",
-  age: 30,
-  isAdmin: true
-};
+let week = ['monday', 'tuesday', 'wednesday', 'thursday',
+    'friday', 'saturday', 'sunday'],
+    
+    today = new Date(2019, 8, 3);
 
-for (let tome in user) {
-  // ключи
-  console.log( tome );  // name, age, isAdmin
-  // значения ключей
-  console.log( user[tome] ); // John, 30, true
-}
+week.forEach (function(item, i, arr) {
+  if (i >= 5) {
+      console.log('%c%s', 'font-style: italic', item);
+  } else  if (i == (today.getDay() - 1) ) {
+      console.log('%c%s', 'font-weight: bold', item);
+  } else {
+      console.log(item);
+  }
+});
