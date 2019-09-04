e125bf6e81fd2a3ed4216ee1e9a935a40c1080aa
Nikita Bukin <locus2@mail.ru>
2019-09-04 12:00:41 +0300

    change week days

--- Lesson06-high/nonObligatory.js
+++ Lesson06-high/nonObligatory.js
@@ -1,12 +1,12 @@
-let week = ['monday', 'tuesday', 'wednesday', 'thursday',
-    'friday', 'saturday', 'sunday'],
+let week = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday',
+    'friday', 'saturday'],
     
     today = new Date();
 
 week.forEach (function(item, i, arr) {
-  if (i >= 5) {
+  if (i == 6 || i == 0) {
       console.log('%c%s', 'font-style: italic', item);
-  } else  if (i == (today.getDay() - 1) ) {
+  } else  if (i == (today.getDay()) ) {
       console.log('%c%s', 'font-weight: bold', item);
   } else {
       console.log(item);
