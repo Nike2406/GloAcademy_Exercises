925569d65a3510850abd90b1f7499aa8bbf34d40
Nikita Bukin <locus2@mail.ru>
2019-09-06 01:54:01 +0300

    add decription

--- Lesson07-extra/script.js
+++ Lesson07-extra/script.js
@@ -5,4 +5,6 @@ do{
     let newEl = document.createElement('li');
     newEl.textContent = prompt('Введите новый пункт', 5);
     newLi[0].appendChild(newEl);
-} while(confirm('One more?'));
+} while(confirm('One more?')); 
+
+/* как сделать, чтобы вывод был в режиме реального времени?*/
