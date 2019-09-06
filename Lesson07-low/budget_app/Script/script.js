73f6bd8efb08724e65b5d76cffb853dd997ad62e
Nikita Bukin <locus2@mail.ru>
2019-09-06 18:08:48 +0300

    change var-s

--- Lesson07-low/budget_app/Script/script.js
+++ Lesson07-low/budget_app/Script/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-let startFinder = document.getElementById('start'),
-    pbIncomeAdd = document.getElementsByClassName('income_add'),
-    pbExpensesAdd = document.getElementsByClassName('expenses_add'),
-    checkBox = document.querySelector('#deposit-check'), 
-    additionalIncome = document.querySelectorAll('.additional_income-item'),
+let start = document.getElementById('start'),
+    btnPlus = document.getElementsByTagName('button'),
+    incomePlus = btnPlus[0],
+    expensesPlus = btnPlus[1],
+    depositeCheck = document.querySelector('#deposit-check'), 
+    additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
 
     budgetDayValue = document.querySelector('.budget_day-value'),
+    budgetMonthValue = document.querySelector('.budget_month-value'),
     expensesMonthValue = document.querySelector('.expenses_month-value'),
     additionalIncomeValue = document.querySelector('.additional_income-value'),
     additionalExpensesValue = document.querySelector('.additional_expenses-value'),
@@ -20,14 +22,13 @@ let startFinder = document.getElementById('start'),
     expensesAmount = document.querySelector('.expenses-amount'),
     additionalExpensesItem = document.querySelector('.additional_expenses-item'),
     targetAmount = document.querySelector('.target-amount'),
-    periodSelet = document.querySelector('.period-select'),
-    monthValue = document.querySelector('.budget_month-value');
+    periodSelet = document.querySelector('.period-select');
     
-console.log('startFinder: ', startFinder);
-console.log('pbIncomeAdd: ', pbIncomeAdd);
-console.log('pbExpensesAdd: ', pbExpensesAdd);
-console.log('checkBox: ', checkBox);
-console.log('additionalIncome: ', additionalIncome);
+console.log('start: ', start);
+console.log('incomePlus: ', incomePlus);
+console.log('expensesPlus: ', expensesPlus);
+console.log('depositeCheck: ', depositeCheck);
+console.log('additionalIncomeItem: ', additionalIncomeItem);
 console.log('budgetDayValue: ', budgetDayValue);
 console.log('expensesMonthValue: ', expensesMonthValue);
 console.log('additionalIncomeValue: ', additionalIncomeValue);
@@ -42,4 +43,4 @@ console.log('expensesAmount: ', expensesAmount);
 console.log('additionalExpensesItem: ', additionalExpensesItem);
 console.log('targetAmount: ', targetAmount);
 console.log('periodSelet: ', periodSelet);
-console.log('monthValue: ', monthValue);
+console.log('budgetMonthValue: ', budgetMonthValue);
