e914591ecd75045d1a6970321517de3ef8471e39
Nikita Bukin <locus2@mail.ru>
2019-09-10 01:38:08 +0300

    add up to cssText

--- Lesson10-low-1/Obligatory.js
+++ Lesson10-low-1/Obligatory.js
@@ -1,9 +1,32 @@
 'use strict';
 
-function DomElement (selector, height, width, bg, fontSize) {
+function DomElement(selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
 }
+
+DomElement.prototype.addSel = function () {
+    if (this.selector[0] == '.') {
+        let div = document.createElement('div');
+        div.className = this.selector;
+        div.innerHTML = this.selector;
+        document.body.append(div);
+    }
+    if (this.selector[0] == '#') {
+        let p = document.createElement('p');
+        p.className = this.selector;
+        p.innerHTML = this.selector;
+        document.body.append(p);
+    }
+};
+let elem = new DomElement('#Hello', '100', '100', 'red', '24px');
+elem.addSel();
+let cssElem = document.getElementsByClassName(`${elem.selector}`);
+// cssElem.style.cssText = `height: ${elem.height}; width: ${elem.width}; 
+// background: ${elem.bg}; font-size: ${elem.fontSize}`;
+console.log(elem);
+console.log(cssElem);
+console.log(`${elem.selector}`);
