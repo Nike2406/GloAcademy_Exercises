bf5b3a21c694ad8ea30e76547ed078531d30f479
Nikita Bukin <locus2@mail.ru>
2019-09-10 14:07:48 +0300

    add to p3

--- Lesson10-low-1/Obligatory.js
+++ Lesson10-low-1/Obligatory.js
@@ -11,22 +11,26 @@ function DomElement(selector, height, width, bg, fontSize) {
 DomElement.prototype.addSel = function () {
     if (this.selector[0] == '.') {
         let div = document.createElement('div');
-        div.className = this.selector;
-        div.innerHTML = this.selector;
+        div.className = this.selector.slice(1, (this.selector).length);
+        div.innerHTML = this.selector;//любой текст
         document.body.append(div);
     }
     if (this.selector[0] == '#') {
         let p = document.createElement('p');
-        p.className = this.selector;
-        p.innerHTML = this.selector;
+        p.className = this.selector.slice(1, (this.selector).length);
+        p.innerHTML = this.selector; //любой текст
         document.body.append(p);
     }
 };
+
 let elem = new DomElement('#Hello', '100', '100', 'red', '24px');
 elem.addSel();
-let cssElem = document.getElementsByClassName(`${elem.selector}`);
-// cssElem.style.cssText = `height: ${elem.height}; width: ${elem.width}; 
-// background: ${elem.bg}; font-size: ${elem.fontSize}`;
-console.log(elem);
-console.log(cssElem);
-console.log(`${elem.selector}`);
+
+let cssElem = document.getElementsByClassName((`${elem.selector}`).slice(1, (`${elem.selector}`).length))[0];
+
+cssElem.style.cssText = `height: ${elem.height}; width: ${elem.width}; 
+ background-color: ${elem.bg}; font-size: ${elem.fontSize}`;
+ 
+ let newElem = new DomElement('.Bye', '200', '200', 'green', '36px'); //новый элемент
+ console.log('newElem: ', newElem); 
+ newElem.addSel(); //вызывем метод класса
