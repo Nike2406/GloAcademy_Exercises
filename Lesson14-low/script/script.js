fda567aa377afa81006ac10713f2c357a5732301
Nikita Bukin <locus2@mail.ru>
2019-09-18 01:45:34 +0300

    menu works till li

--- Lesson14-low/script/script.js
+++ Lesson14-low/script/script.js
@@ -57,33 +57,47 @@ window.addEventListener('DOMContentLoaded', function () {
             menu = document.querySelector('menu'),
             closeBtn = document.querySelector('.close-btn'),
             menuItems = menu.querySelectorAll('ul>li'),
-          handlerMenu = () => {
+            body = document.querySelector('body'),
+            // handlerMenu = () => {
 
-        if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
-            
+            //     if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+
+            //         menu.classList.toggle('active-menu');
+            //     } else {
+            //         if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+            //             menu.style.transform = `translate(0)`;
+            //         } else {
+            //             menu.style.transform = `translate(-100%)`;
+            //         }
+            //     }
+
+            // };
+            handlerMenu = () => {
                 menu.classList.toggle('active-menu');
-            }  else {
-                if(!menu.style.transform || menu.style.transform === `translate(-100%)`) {
-                    menu.style.transform = `translate(0)`;
-                } else {
-                        menu.style.transform = `translate(-100%)`;
+            };
+
+            body.addEventListener('click', (event) => {
+                let target = event.target;
+                console.log('target: ', target);
+
+                if (target.classList.contains('menu') || target.closest('.menu') ||
+                target.classList.contains('close-btn')) {
+                    menu.classList.toggle('active-menu');
+                } else if (menu.classList.contains('active-menu') && !(target.closest('.active-menu'))) {
+                    menu.classList.toggle('active-menu');
                 }
-            }
-//mark
-//Hardcore
-// if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) && 
-// window.innerWidth >= 1000 && !menu.contains('active-menu') && menu.style.transform !== `translate(0)`) {
-//     menu.classList.toggle('active-menu');
-// } else if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) && 
-// window.innerWidth >= 1000 && menu.contains('active-menu') && menu.style.transform !== `translate(0)`) {
-//     menu.classList.toggle('active-menu');
-// }
+                // else {
+                //     target = target.closest('.active-menu');
 
-        };
+                //     if (!target) {
+                //         menu.classList.toggle('active-menu');
+                //     }
+                // }
+            });
 
-        btnMenu.addEventListener('click', handlerMenu);
-        closeBtn.addEventListener('click', handlerMenu);
-        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+        // btnMenu.addEventListener('click', handlerMenu);
+        // closeBtn.addEventListener('click', handlerMenu);
+        // menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
 
     };
     toggleMenu();
@@ -92,89 +106,130 @@ window.addEventListener('DOMContentLoaded', function () {
     const togglePopUp = () => {
         let popUp = document.querySelector('.popup'),
             popupBtn = document.querySelectorAll('.popup-btn'),
-            popUpClose = document.querySelector('.popup-close'),
             popUpTab = document.querySelector('.popup-content');
 
 
-    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) && 
-    window.innerWidth >= 1000) {
-         console.log('window.innerWidth: ', window.innerWidth);
-         
+        if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) &&
+            window.innerWidth >= 1000) {
 
-        popupBtn.forEach((elem) => {
-            elem.addEventListener('click', () => {
-                popUp.style.display = 'block';
-                popUpTab.style.top = `-${popUpTab.offsetHeight}px`;
-                let i= -popUpTab.offsetHeight,
+            popupBtn.forEach((elem) => {
+                elem.addEventListener('click', () => {
+                    popUp.style.display = 'block';
+                    popUpTab.style.top = `-${popUpTab.offsetHeight}px`;
+                    let i = -popUpTab.offsetHeight,
+                        showUpTab,
+
+                        tabAnimate = () => {
+                            showUpTab = requestAnimationFrame(tabAnimate);
+                            i += 10;
+                            if (i < 60) {
+                                popUpTab.style.top = i + 'px';
+                            } else {
+                                cancelAnimationFrame(showUpTab);
+                            }
+                        };
+                    requestAnimationFrame(tabAnimate);
+
+                });
+            });
+
+            popUp.addEventListener('click', () => {
+                const tabBelow = popUpTab.offsetHeight + popUpTab.offsetTop;
+                let j = popUpTab.offsetTop,
+                    closeUpTab,
+                    target = event.target,
+                    tabAnimate = () => {
+                        closeUpTab = requestAnimationFrame(tabAnimate);
+
+                        if (j > -(tabBelow)) {
+                            popUpTab.style.top = j + 'px';
+                            j -= 10;
+                        } else {
+                            cancelAnimationFrame(closeUpTab);
+                            popUp.style.display = 'none';
+                        }
+                    };
                 
-                showUpTab,
-                tabAnimate = () => {
-                    showUpTab = requestAnimationFrame(tabAnimate);
-                    i += 10;
-                    if (i <  60) {
-                        popUpTab.style.top = i + 'px';
-                    } else {
-                        cancelAnimationFrame(showUpTab);
+
+                if (target.classList.contains('popup-close')) {
+                    requestAnimationFrame(tabAnimate);
+                } else {
+                    target = target.closest('.popup-content');
+
+                    if (!target) {
+                        requestAnimationFrame(tabAnimate);
                     }
-                };
-                showUpTab = requestAnimationFrame(tabAnimate);
+                }
+
                 
             });
-        });
 
-        popUpClose.addEventListener('click', () => {
-            const tabBelow = popUpTab.offsetHeight + popUpTab.offsetTop;
-            let j = popUpTab.offsetTop,
-                closeUpTab,
-            tabAnimate = () => {
-                closeUpTab = requestAnimationFrame(tabAnimate);
-                
-                if (j > -(tabBelow)) {
-                    popUpTab.style.top = j + 'px';
-                    j -= 10;
-                } else {
-                    cancelAnimationFrame(closeUpTab);
+        } else {
+            popupBtn.forEach((elem) => {
+                elem.addEventListener('click', () => {
+                    popUp.style.display = 'block';
+                });
+            });
+
+            //Добавляем общее
+            popUp.addEventListener('click', (event) => {
+                let target = event.target;
+
+                if (target.classList.contains('popup-close')) {
                     popUp.style.display = 'none';
-                }
-            };
-            closeUpTab = requestAnimationFrame(tabAnimate);
-        });
-    } else {
-        popupBtn.forEach((elem) => {
-            elem.addEventListener('click', () => {
-                popUp.style.display = 'block';                
-            });            
-        });
-        
-        popUpClose.addEventListener('click', () => {
-            popUp.style.display = 'none';
-        });
+                } else {
+                    target = target.closest('.popup-content');
 
-    }
+                    if (!target) {
+                        popUp.style.display = 'none';
+                    }
+                }
+            });
+
+        }
+
 
     };
     togglePopUp();
 
-});
+    //Tabs
+    const tabs = () => {
 
-/* Черновик анимации через CSS
-popupBtn.forEach((elem) => {
-            elem.addEventListener('click', () => {
-                popUp.style.display = 'block';
-                popUpTab.style.cssText = `top: -100%`;
-                //popUpTab.style.transform = `translateY(-120%);`;
-                popUpTab.style.cssText = `                
-                transition: all 0.5s ease-in;                
-                transform: translateY(20%)`;
-                console.log('popUpTab: ', popUpTab);
-            });
+        const tabHeader = document.querySelector('.service-header'),
+            tab = document.querySelectorAll('.service-header-tab'),
+            tabContent = document.querySelectorAll('.service-tab');
+
+        const toggleTabContent = (index) => {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (index === i) {
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                }
+            }
+        };
+
+        tabHeader.addEventListener('click', (event) => {
+            let target = event.target;
+            target = target.closest('.service-header-tab');
+
+            if (target) {
+                tab.forEach((item, i) => {
+                    if (item === target) {
+                        toggleTabContent(i);
+                    }
+                });
+            }
+
         });
-        console.dir(popUpTab);
-        popUpClose.addEventListener('click', () => {
-            popUpTab.style.cssText = `                
-                transition: all 0.5s ease-in-out;                
-                transform: translateY(-120%);`;
-            setTimeout(() => {
-                popUp.style.display = 'none';
-            }, 510);
-        });*/
+    };
+    tabs();
+
+    //Проверка
+    // document.addEventListener('click', (elem) => {
+    //     console.log(elem.target);
+    // });
+
+});
