3621a0869b1da51122a8fe36f79cdab125a339d7
Nikita Bukin <locus2@mail.ru>
2019-09-21 16:04:52 +0300

    fulffill ex to p5

---
+++ Lesson15-high-1/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let div = document.querySelectorAll('div');
+div[0].innerHTML =  div[0].innerHTML.replace(/(функц)([а-яё]+)/gi, '<strong>$1$2</strong>' );
+div[1].innerHTML =  div[1].innerHTML.replace(/(\d{2}):(\d{2})/gi, '<strong>$1:$2</strong>' );
+div[1].innerHTML =  div[1].innerHTML.replace(/"(.+\n?.+?)"/mgi, '<mark>$1</mark>');
+div[0].innerHTML =  div[0].innerHTML.replace(/("|«)(.+)("|»)/gi, '<mark>$2</mark>');
+div[1].innerHTML =  div[1].innerHTML.replace(/(\w{4,5}):\/\/(www)?\.?(\w+\.\w{2,3})(.+\/+)?/mgi,
+     '<a href="$1://$3$4">$3</a>');
+
+div.forEach((item) => {
+    let color = item.getAttribute('style').match(/#\w+/gi);
+    console.log('color: ', color);
+});
