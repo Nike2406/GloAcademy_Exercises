fbfbf8f79153f23e87eca4ed3dea34125ed7fb16
Nikita Bukin <locus2@mail.ru>
2019-09-21 16:06:45 +0300

    add decription

--- Lesson15-high-1/script.js
+++ Lesson15-high-1/script.js
@@ -6,7 +6,7 @@ div[1].innerHTML =  div[1].innerHTML.replace(/(\d{2}):(\d{2})/gi, '<strong>$1:$2
 div[1].innerHTML =  div[1].innerHTML.replace(/"(.+\n?.+?)"/mgi, '<mark>$1</mark>');
 div[0].innerHTML =  div[0].innerHTML.replace(/("|«)(.+)("|»)/gi, '<mark>$2</mark>');
 div[1].innerHTML =  div[1].innerHTML.replace(/(\w{4,5}):\/\/(www)?\.?(\w+\.\w{2,3})(.+\/+)?/mgi,
-     '<a href="$1://$3$4">$3</a>');
+     '<a href="$1://$3$4">$3</a>'); //Также является ответом на 6-е задание
 
 div.forEach((item) => {
     let color = item.getAttribute('style').match(/#\w+/gi);
